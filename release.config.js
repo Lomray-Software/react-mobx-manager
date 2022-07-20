659fed248a89e7941f1b56c52868961a8618bf2a
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-07-20 18:02:12 +0300

    ci: publish npm package

--- release.config.js
+++ release.config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
     ["@semantic-release/github", {
       "assets": [
         {"path": "build.zip", "label": "Build-${nextRelease.gitTag}"},
