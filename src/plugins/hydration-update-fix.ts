52ec4698a56a9d7e8060cb2f9ac200e1e402286d
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-07-11 20:58:47 +0200

    fix: hydration plugin

--- src/plugins/hydration-update-fix.ts
+++ src/plugins/hydration-update-fix.ts
@@ -7,12 +7,10 @@ import type { Plugin } from 'vite';
 function HydrationUpdateFix(): Plugin {
   return {
     name: '@lomray/react-mobx-manager-hydration-fix',
-    transform(code, id) {
-      if (!id.includes('mobx-react-lite') || !code.includes('function useObserver')) {
+    transform(code) {
+      if (!code.includes('function useObserver')) {
         return;
       }
-
-      console.log(code.replace(/(}\);[^f]+)(forceUpdate\(\);)/s, '$1/*$2*/'));
 
       return {
         code: code.replace(/(}\);[^f]+)(forceUpdate\(\);)/s, '$1/*$2*/'),
