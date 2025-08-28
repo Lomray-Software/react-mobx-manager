8d98ac95a749602f3bdc7b743e2971b792f40d85
MatthewPattell <mikhail.yarmaliuk@lomray.com>
2025-08-28 16:55:27 +0800

    fix: persisting store

--- src/storages/combined-storage.ts
+++ src/storages/combined-storage.ts
@@ -161,6 +161,10 @@ class CombinedStorage implements IStorage {
         return null;
       }
 
+      if (!this.persistData[storageId]) {
+        this.persistData[storageId] = {};
+      }
+
       if (!this.persistData[storageId]?.[storeId]) {
         this.persistData[storageId][storeId] = {};
       }
