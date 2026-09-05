6b26597d551a308fff85e7d1e1a4f0d261301741
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2026-09-05 03:08:40 -0700

    fix: replace array contents when restoring state

--- src/deep-merge.ts
+++ src/deep-merge.ts
@@ -11,9 +11,19 @@ const deepMerge = (target: any, source: any): boolean => {
     return false;
   }
 
+  if (Array.isArray(target) && Array.isArray(source)) {
+    target.splice(0, target.length, ...(source as unknown[]));
+
+    return true;
+  }
+
   for (const key in source) {
     if (target.hasOwnProperty(key)) {
-      if (isObject(target[key]) && isObject(source[key])) {
+      if (
+        isObject(target[key]) &&
+        isObject(source[key]) &&
+        Array.isArray(target[key]) === Array.isArray(source[key])
+      ) {
         deepMerge(target[key] as Record<string, any>, source[key] as Record<string, any>);
       } else {
         target[key] = source[key];
